9130704e5c2972f44144294b0b2ba93b3dd294cb
Eusky <yoochun8128@gmail.com>
2025-08-08 17:49:42 +0900

    마이페이지 ui 작업

--- puppit/src/main/java/org/puppit/service/PointServiceImpl.java
+++ puppit/src/main/java/org/puppit/service/PointServiceImpl.java
@@ -2,9 +2,11 @@ package org.puppit.service;
 
 import org.puppit.repository.PointDAO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class PointServiceImpl implements PointService {
@@ -13,10 +15,19 @@ public class PointServiceImpl implements PointService {
 
   @Override
   public boolean verifyAndCharge(int uid, int amount) {
-     boolean resultPointUpdate =  pointDAO.updatePoint(uid, amount) == 1;
-     boolean resultRecordUpdate = pointDAO.updatePointRecord(uid, amount) == 1;
+    try {
+      boolean resultPointUpdate = pointDAO.updatePoint(uid, amount) == 1;
+      boolean resultRecordUpdate = pointDAO.updatePointRecord(uid, amount) == 1;
+      
+      if(!resultPointUpdate || !resultRecordUpdate) {
+        throw new RuntimeException("포인트 잔액 충전 실패");
+      }
+      
+    } catch (Exception e) {
+      throw new RuntimeException("포인트 충전 중 오류가 발생했습니다.");
+    }
      
-     return resultPointUpdate && resultRecordUpdate;
+     return true;
 
   }
 }
