63a64b02da6743229e6be2e9c6a6ecae3918ce09
jinseong125 <jinseong125@gmail.com>
2025-08-11 15:49:56 +0900

    메인,헤더.jsp 수정

--- puppit/src/main/java/org/puppit/controller/MainController.java
+++ puppit/src/main/java/org/puppit/controller/MainController.java
@@ -1,7 +1,11 @@
 package org.puppit.controller;
 
+import org.puppit.model.dto.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -9,8 +13,15 @@ public class MainController {
   @RequestMapping(value = "/")
   public String main() {
     return "main";
-    
   }
+  
+  @GetMapping("user/login")
+  public String userlogin() {
+    return "user/login";
+  }
+
+
+  
  
   
 }
