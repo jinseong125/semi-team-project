15b49ae78cd3609ec46f0d6666653bc6761e0274
ziwk13 <ziwk13@hanmail.com>
2025-08-11 14:19:31 +0900

    로그인 & 로그 정보 DB insert 구현

--- puppit/src/main/java/org/puppit/controller/UserController.java
+++ puppit/src/main/java/org/puppit/controller/UserController.java
@@ -1,6 +1,13 @@
 package org.puppit.controller;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpSession;
+
 import org.puppit.model.dto.UserDTO;
+import org.puppit.model.dto.UserStatusDTO;
 import org.puppit.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,5 +48,39 @@ public class UserController {
     // 회원가입 성공
     redirectAttr.addFlashAttribute("msg", "Puppit에 오신것을 환영 합니다");
     return "redirect:/";
+  }
+  
+  @GetMapping("/login")
+  public String loginForm() {
+    return "user/login";
+  }
+  
+  @PostMapping("/login")
+  public String login(UserDTO user, HttpSession session, RedirectAttributes redirectAttr) {
+    try {
+      boolean loginResult = userService.login(user);
+      
+      // timeStamp 생성
+      Date now = new Date();
+      SimpleDateFormat stf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+      String dateTimeStr = stf.format(now);
+      Date date = stf.parse(dateTimeStr);
+      Timestamp timestamp = new Timestamp(date.getTime());
+      Integer userId = userService.getUserId(user.getAccountId());
+      UserStatusDTO userLog = new UserStatusDTO(user.getAccountId(), userId, timestamp);
+      if(!loginResult) {
+        redirectAttr.addFlashAttribute("error", "아이디나 비밀번호를 확인 해주세요");
+        return "redirect:/user/login";
+      }
+      session.setAttribute("accountId", user.getAccountId());
+      redirectAttr.addFlashAttribute("msg", "로그인 성공!");
+      
+      userService.insertLogStatus(userLog);
+      return "redirect:/";
+    } catch (Exception e) {
+       e.printStackTrace();
+       return "redirect:/user/login";
+    }
+    
   }
 }
--- puppit/src/main/java/org/puppit/service/UserService.java
+++ puppit/src/main/java/org/puppit/service/UserService.java
@@ -2,11 +2,15 @@ package org.puppit.service;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.puppit.model.dto.UserDTO;
+import org.puppit.model.dto.UserStatusDTO;
 
 @Mapper
 public interface UserService {
   
   boolean signup(UserDTO user); 
   boolean countByAccountId(String accountId);
+  boolean login(UserDTO user);
+  Integer insertLogStatus(UserStatusDTO log);
+  Integer getUserId(String accountId);
   
 }
